18c5fe0f35c5bc6202d45c30c9d6fd6c472ad541
Feng <chiafeng1992@gmail.com>
2018-08-30 11:08:51 +0800

    1. Reindent the code. 2. Compare to many people at once 3. Increase training data amount. TODO: put these data into one batch. 4. Increase testing data amount(used to measure how good is the model).

--- myauto.py
+++ myauto.py
@@ -7,34 +7,49 @@ import preprocess
 tf.reset_default_graph()
 
 learning_rate = 0.01
-training_epochs = 10000
-display_step = 100          # how many steps to display a result
+training_epochs = 300
+display_step = 10          # how many steps to display a result
 
 hidden_state_size = 100
-samples = 1
-time_steps = 200 
-step_dims = 1894          # remove timestamp for test
+samples = 100
+time_steps = 50
+step_dims = 681          # remove timestamp for test
 
-save_dir = "/Users/feng/Desktop/ML/Tensorflow-seq2seq-autoencoder/save_{}/"\
-           .format(dt.datetime.now().strftime("%m%d%H%M"))
+# file path of training & testing data
+regular_data_file = '~/Desktop/user_ml/tony_hsieh.1.csv'
+compare_data_files = {
+					  'AW': '~/Desktop/user_ml/amy_wu.1.csv',
+					  'AC': '~/Desktop/user_ml/angus_chang.1.csv',
+					  'BK': '~/Desktop/user_ml/ben_ko.1.csv',
+					  'CH': '~/Desktop/user_ml/carl_huang.1.csv',
+					  'JC': '~/Desktop/user_ml/jack_chang.1.csv',
+					  'J': '~/Desktop/user_ml/jerry_wu.1.csv',
+					  'KL': '~/Desktop/user_ml/kozue_lai.1.csv',
+					  'MS': '~/Desktop/user_ml/michelle_song.1.csv',
+					  #'TH': '~/Desktop/user_ml/tony_hsieh.1.csv',
+					  'Y': '~/Desktop/user_ml/yiyi_lyu.1.csv',
+					  'YF': '~/Desktop/user_ml/yvonne_feng.1.csv',
+					  #'K': '~/Desktop/user_ml/kevin_chiu.1.csv',
+					 }
 
-data = preprocess.get_batch('vector/angus_chang_201807.data.angus_chang',
-                            time_steps*2, step_dims)
-# slice data from 2nd dimension
-train_data = data[:, :time_steps, :]
-test_data = data[:, time_steps:time_steps*2, :]
+save_dir = "save_{}/".format(dt.datetime.now().strftime("%m%d%H%M"))
 
-J_data = preprocess.get_batch('vector/jerry_wu_201807.data.jerry_wu',
-                              time_steps, step_dims)
-# K_data = preprocess.get_batch('vector/kevin_chiu_201807.data.kevin_chiu',
-#                               time_steps, step_dims)
-Y_data = preprocess.get_batch('vector/yiyi_lyu_201807.data.yiyi_lyu',
-                              time_steps, step_dims)
+# get 'samples' train_data and 3 test_data
+data = preprocess.get_batch(regular_data_file, samples+3, time_steps, step_dims)
+train_data = data[:samples]
+test_data = data[samples:]
+
+# get one data for other people
+cmp_data = {}
+for name, file in compare_data_files.items():
+	cmp_data[name] = preprocess.get_batch(file, 1, time_steps, step_dims)[0]
 
 ##### Defining graph #####
-seq_input = tf.placeholder(tf.float32, [samples, time_steps, step_dims])
+# TODO: use shape [None, time_steps, step_dims]
+seq_input = tf.placeholder(tf.float32, [1, time_steps, step_dims])
 
 encoder_inputs = tf.unstack(seq_input)
+print(len(encoder_inputs), encoder_inputs[0].shape)		#samples, (time_steps, step_dims)
 
 layers = [tf.contrib.rnn.BasicLSTMCell(units) for units in [hidden_state_size, step_dims]]
 enc_cell = tf.nn.rnn_cell.MultiRNNCell(layers)
@@ -46,66 +61,69 @@ y_pred = dec_outputs
 
 loss = 0
 for i in range(len(y_true)):
-       loss += tf.reduce_sum(tf.square(tf.subtract(y_pred[i], y_true[i])))
+	loss += tf.reduce_sum(tf.square(tf.subtract(y_pred[i], y_true[i])))
 # summary for FileWriter
 loss_summary = tf.summary.scalar("loss", loss)
 optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
 merged_summary = tf.summary.merge_all()
 
-##### Start training #####
+# setup model saver and writer
 saver = tf.train.Saver()
 train_writer = tf.summary.FileWriter(save_dir+"train_plot", tf.get_default_graph())
-test_writer = tf.summary.FileWriter(save_dir+"test_plot", tf.get_default_graph())
-J_writer = tf.summary.FileWriter(save_dir+"J", tf.get_default_graph())
-K_writer = tf.summary.FileWriter(save_dir+"K", tf.get_default_graph())
-Y_writer = tf.summary.FileWriter(save_dir+"Y", tf.get_default_graph())
+test_writer = []
+for i, data in enumerate(test_data):
+	test_writer.append(tf.summary.FileWriter(save_dir+"test"+str(i+1), tf.get_default_graph()))
 
+# setup writers of other people
+cmp_writer = {}
+for name in compare_data_files.keys():
+	cmp_writer[name] = tf.summary.FileWriter(save_dir+name, tf.get_default_graph())
+
+##### Start training #####
 with tf.Session() as sess:
-       sess.run(tf.global_variables_initializer())
+	sess.run(tf.global_variables_initializer())
 
-       print("start training....\nsave_dir={}\nlearning rate: {}\n".format(save_dir, learning_rate))
-       x = train_data
-       for epoch in range(training_epochs):
-           feed = {seq_input: x}
-           _, cost_value, summary = sess.run([optimizer, loss, merged_summary], feed_dict=feed)
-           if epoch % display_step == 0:
-               print ("logits")
-               a = sess.run(y_pred, feed_dict=feed)
-               print (a)
-               print ("labels")
-               b = sess.run(y_true, feed_dict=feed)
-               print (b)
+	print("start training....\nsave_dir={}\nlearning rate: {}\n".format(save_dir, learning_rate))
+	x = train_data
+	for epoch in range(training_epochs):
+		# TODO: always use same batch
+		# circular repeat the training data
+		feed = {seq_input: x[epoch%samples]}
+		_, cost_value, summary, out = sess.run([optimizer, loss, merged_summary, dec_outputs], feed_dict=feed)
+		if epoch % display_step == 0:
 
-               print("Epoch:" + '%04d' % (epoch+1), "cost=" + "{:.9f}".format(cost_value))
+			print(len(out), out[0].shape)
 
-               train_writer.add_summary(summary, epoch)
+			print ("logits")
+			a = sess.run(y_pred, feed_dict=feed)
+			print (a)
+			print ("labels")
+			b = sess.run(y_true, feed_dict=feed)
+			print (b)
 
-               saver.save(sess, save_dir + "model.ckpt")
+			print("Epoch:" + '%04d' % (epoch+1), "cost=" + "{:.9f}".format(cost_value))
 
-               # calculate loss for test data
-               feed = {seq_input: test_data}
-               cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
-               test_writer.add_summary(summary, epoch)
+			train_writer.add_summary(summary, epoch)
 
-               # calculate loss for J
-               feed = {seq_input: J_data}
-               cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
-               J_writer.add_summary(summary, epoch)
+			saver.save(sess, save_dir + "model.ckpt")
 
-               # calculate loss for K
-               # feed = {seq_input: K_data}
-               # cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
-               # K_writer.add_summary(summary, epoch)
+			# calculate loss for test data
+			for i, data in enumerate(test_data):
+				feed = {seq_input: data}
+				cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
+				test_writer[i].add_summary(summary, epoch)
 
-               # calculate loss for Y
-               feed = {seq_input: Y_data}
-               cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
-               Y_writer.add_summary(summary, epoch)
+			# calculate loss for cmp_data
+			for name, data in cmp_data.items():
+				feed = {seq_input: data}
+				cost_value, summary = sess.run([loss, merged_summary], feed_dict=feed)
+				cmp_writer[name].add_summary(summary, epoch)
 
 print("Optimization Finished!")
+
 train_writer.close()
-test_writer.close()
-J_writer.close()
-K_writer.close()
-Y_writer.close()
+for writer in test_writer:
+	writer.close()
+for writer in cmp_writer.values():
+	writer.close()
--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import Imputer, StandardScaler
 
-def get_batch(filedir, time_steps, step_dims, offset=0):
+def get_batch(filedir, batch_size, time_steps, step_dims):
 	dataset = pd.read_csv(filedir, header=None, dtype=np.float32)
 	X = dataset.values
 	X[np.isnan(X)] = 0
@@ -10,16 +10,27 @@ def get_batch(filedir, time_steps, step_dims, offset=0):
 	# discard timestamp
 	X = X[:, 1:]
 
-	# discard useless part
-	X = X[offset:time_steps]
+	#TODO: check X.shape is match step_dims
 
 	# standardization
 	scaler = StandardScaler()
 	X = scaler.fit_transform(X)		# StandardScaler expect dim <= 2
 
-	X = np.reshape(X, [1, time_steps, step_dims])	# [samples, time_steps, step_dims]
-	print(X.shape)
-	# X = X.transpose(1, 0, 2)		# exchange 0th and 1st dimension for RNN input
-	# print(X.shape)
+	# truncate last part to let X reshape into (?, time_steps, step_dims)
+	remainder = X.shape[0] % time_steps
+	X = X[:(X.shape[0]-remainder)]
+	X = np.reshape(X, [-1, time_steps, step_dims])
 
-	return X
+	# randomly select batch_size samples from X
+	np.random.seed(27)
+	batch_indices = np.random.permutation(len(X))[:batch_size]
+	X = X[batch_indices]
+	batch_data = []
+	for instance in X:
+		batch_data.append(np.reshape(instance, [1, time_steps, step_dims]))
+
+	# batch_data is list of array
+	# len(batch_data) == batch_size
+	# batch_data[i].shape == (1, time_steps, step_dims)
+
+	return batch_data
